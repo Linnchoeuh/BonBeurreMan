48024844af38ad37a7743b6caf1e700d4a6d6d85
Linnchoeuh <lennintendo@gmail.com>
2020-12-07 22:05:46 +0100

    moving and rename the key input file

--- bonbeurreman.py
+++ bonbeurreman.py
@@ -14,6 +14,7 @@ except:
 import includes.player as player
 import includes.bomb as bomb
 import includes.mapdisplayer as mapdisplayer
+import includes.keyboard_input_detect as keyboard_input_detect
 import ctypes
 from os import listdir
 from os.path import isfile, join
@@ -43,6 +44,7 @@ menu = 0 #Menu d'entrée
 load_menu = 0 #Variable permettant de charger a la première frame du menu actif
 
 mouse_click_left = True # Variable a utiliser si l'on veut que l'utilisateur relache le click
+# keyboard_input = KeyBoardKeyDetect.keyboard_input_fonc() 
 keyboard_input = {
     "z" : False,
     "s" : False,
@@ -58,7 +60,7 @@ keyboard_input = {
     "ESCAPE" : False
 } #Dictionnaire des touches pressables
 
-fichiers = [f for f in listdir("levels") if isfile(join("levels", f))] #Liste les niveau stocké dans le dossier levels, et seulement ceux finissant par .data
+fichiers = [f for f in listdir("levels") if isfile(join("levels", f))] #Liste les niveaux stocké dans le dossier levels, et seulement ceux qui contiennent .data dans leur nom
 temp = []
 for i in range(len(fichiers)):
     if fichiers[i].find(".data") != -1:
@@ -68,21 +70,7 @@ fichiers = temp
 
 md = mapdisplayer.Mapdislayer()
 result = "ok"
-
-# key_name = [
-#     "K_z",
-#     "K_s",
-#     "K_q",
-#     "K_d",
-#     "K_UP",
-#     "K_DOWN",
-#     "K_LEFT",
-#     "K_RIGHT",
-#     "K_SPACE",
-#     "K_LSHIFT",
-#     "K_RETURN",
-#     "K_ESCAPE"
-# ]
+level_select_offset = 0
     
 # Génération des fonctions -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -109,6 +97,16 @@ def collision_rect(x, y, width, height, text = False, click_block_condition = Fa
 
     return (touched, pressed)
 
+def collision_rect_texture(x, y, texture, texture_rect, click_block_condition = False): # Permet de creer une zone de collision avec une image fixe(position x, position y, texture, sa zone de collisions,
+    pressed = False                                                     # empêcher l'activation du boutton si le click gauche de la souris est déjà pressé
+    window_surface.blit(texture, res_pos(x,y))
+    # print(texture_rect.collidepoint(mx,my))
+    if mousepress[0] and mouse_click_left == True and texture_rect.collidepoint(mx,my) == 1 or mousepress[0] and click_block_condition == True and texture_rect.collidepoint(mx,my) == 1:
+        pressed = True
+    # if devmode == True:
+    #     pygame.draw.rect(window_surface, white, pygame.Rect(res_pos(x)[0], res_pos(0,y)[1], res_pos(width)[0], res_pos(0,height)[1]), 1)
+    return pressed
+
 pygame.init() # Lancement de pygame -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 clock = pygame.time.Clock()
@@ -123,9 +121,15 @@ text_40a = pygame.font.Font(join("fonts", "arialbd.ttf"), round(res_adaptation(6
 text_150a = pygame.font.Font(join("fonts", "arialbd.ttf"), round(res_adaptation(150)))
 
 #Chargement des textures
-warn = pygame.image.load(f"img/ui/warn.png").convert_alpha() #Fonction pour importe l'image
+warn = pygame.image.load(f"img/ui/warn.png").convert_alpha() #Fonction pour importer l'image
 warn = pygame.transform.smoothscale(warn, res_pos(220,200)) #N'utiliser pas smooth scale sur une pixel art car ca la rendrait flou
 # warn = pygame.transform.scale(warn, res_pos(220,200)) #Fonction pour la scale a la résolution d'affichage
+
+left_arrow = pygame.image.load(f"img/ui/left_arrow.png").convert_alpha()
+left_arrow = pygame.transform.smoothscale(left_arrow, res_pos(95,180))
+
+right_arrow = pygame.image.load(f"img/ui/right_arrow.png").convert_alpha()
+right_arrow = pygame.transform.smoothscale(right_arrow, res_pos(95,180))
 
 
 
@@ -137,109 +141,55 @@ while launched: # Pour fermer la fenêtre
         if event.type == pygame.QUIT:
             launched = False
     
-    pressing = pygame.key.get_pressed()
-    
-    #z
-    if pressing[pygame.K_z]:
-        keyboard_input["z"] = True
-    else:
-        keyboard_input["z"] = False
-
-    #s
-    if pressing[pygame.K_s]:
-        keyboard_input["s"] = True
-    else:
-        keyboard_input["s"] = False   
-
-    #q
-    if pressing[pygame.K_q]:
-        keyboard_input["q"] = True
-    else:
-        keyboard_input["q"] = False
-
-    #d
-    if pressing[pygame.K_d]:
-        keyboard_input["d"] = True
-    else:
-        keyboard_input["d"] = False
-
-    #UP
-    if pressing[pygame.K_UP]:
-        keyboard_input["UP"] = True
-    else:
-        keyboard_input["UP"] = False
-
-    #DOWN
-    if pressing[pygame.K_DOWN]:
-        keyboard_input["DOWN"] = True
-    else:
-        keyboard_input["DOWN"] = False
-
-    #LEFT
-    if pressing[pygame.K_LEFT]:
-        keyboard_input["LEFT"] = True
-    else:
-        keyboard_input["LEFT"] = False
-
-    #RIGHT
-    if pressing[pygame.K_RIGHT]:
-        keyboard_input["RIGHT"] = True
-    else:
-        keyboard_input["RIGHT"] = False
-
-    #SPACE
-    if pressing[pygame.K_SPACE]:
-        keyboard_input["SPACE"] = True
-    else:
-        keyboard_input["SPACE"] = False
-
-    #LSHIFT
-    if pressing[pygame.K_LSHIFT]:
-        keyboard_input["LSHIFT"] = True
-    else:
-        keyboard_input["LSHIFT"] = False
-
-    #RETURN
-    if pressing[pygame.K_RETURN]:
-        keyboard_input["RETURN"] = True
-    else:
-        keyboard_input["RETURN"] = False
-
-    #ESCAPE
-    if pressing[pygame.K_ESCAPE]:
-        keyboard_input["ESCAPE"] = True
-    else:
-        keyboard_input["ESCAPE"] = False
+    keyboard_input = keyboard_input_detect.keyboard_input_fonc(pygame)
+    # print(keyboard_input)
     
     mx, my = pygame.mouse.get_pos()
     mousepress = pygame.mouse.get_pressed()
-    # print(keyboard_input)
+    
     window_surface.fill(black)
     
     if menu == 0: #Menu principal -------------------------------------------------------------------------------------------------------------------------------------------------------------
         if load_menu != 0: # Mettez ici les éléments a charger une seule fois
             mouse_click_left = False
             load_menu = 0
-        if collision_rect(810, 375, 300, 150, "Jouer")[1] == True:
+        if collision_rect(810, 375, 300, 150, "Jouer")[1] == True: #Passe sur le menu de selection de niveau
             menu = 1
-        if collision_rect(810, 600, 300, 150, "Options")[1] == True:
-            # menu = 2
-            pass
-        if collision_rect(810, 825, 300, 150, "Quitter")[1] == True:
+            level_select_offset = 0
+        if collision_rect(810, 600, 300, 150, "Options")[1] == True: #Passe sur les options
+            menu = 2
+        if collision_rect(810, 825, 300, 150, "Quitter")[1] == True: #Ferme le jeu
             launched = False
     
     if menu == 1: #Selection du niveau --------------------------------------------------------------------------------------------------------------------------------------------------------
         if load_menu != 1: # Mettez ici les éléments a charger une seule fois
             mouse_click_left = False
+            left_arrow_rect = left_arrow.get_rect(topleft=res_pos(25,350))
+            right_arrow_rect = right_arrow.get_rect(topleft=res_pos(1800,350))
             load_menu = 1
         
         window_surface.blit(text_150a.render("Choisissez votre niveau", True, white), res_pos(60,0))
 
-        for i in range(len(fichiers)):
-            if collision_rect(100+(i*325), 300, 265, 300, f"{fichiers[i]}")[1] == True:
+        if level_select_offset*5 < len(fichiers)-1:
+            if collision_rect_texture(1800, 360, right_arrow, right_arrow_rect) == True:
+                level_select_offset += 1
+        if level_select_offset*5 >= 4:
+            if collision_rect_texture(25, 360, left_arrow, left_arrow_rect) == True:
+                level_select_offset -= 1
+
+
+
+        k = i = level_select_offset*5
+        if i < 0:
+            i = 0
+        while i <= k+4: #Affiche les niveaux
+            if i+1 > len(fichiers):
+                break
+            if collision_rect(160+i*325-k*325, 300, 265, 300, f"{fichiers[i]}")[1] == True:
                 result = md.load(fichiers[i])
                 if result == "Invalid extension" or result == "Corrupted map":
-                    menu = 2
+                    menu = 10
+            i += 1
         
         if collision_rect(0, 975, 300, 105, "Retour")[1] == True:
             menu = 0
@@ -248,7 +198,15 @@ while launched: # Pour fermer la fenêtre
         if collision_rect(490, 975, 675, 105, "Connexion multi local")[1] == True:
             pass
     
-    if menu == 2:
+    if menu == 2: #Options --------------------------------------------------------------------------------------------------------------------------------------------------------
+        if load_menu != 2: # Mettez ici les éléments a charger une seule fois
+            mouse_click_left = False
+            load_menu = 2
+        if collision_rect(0, 975, 300, 105, "Retour")[1] == True:
+            menu = 0    
+    
+    
+    if menu == 10:
         window_surface.blit(warn, res_pos(850,100))
         if result == "Invalid extension":
             window_surface.blit(text_40a.render("Le fichier n'as pas pu être chargé car il est inexistant", True, red), res_pos(195,450))  
---
+++ includes/keyboard_input_detect.py
@@ -0,0 +1,116 @@
+keyboard_input = {
+    "z" : False,
+    "s" : False,
+    "q" : False,
+    "d" : False,
+    "UP" : False,
+    "DOWN" : False,
+    "LEFT" : False,
+    "RIGHT" : False,
+    "SPACE" : False,
+    "LSHIFT" : False,
+    "RETURN" : False,
+    "ESCAPE" : False
+}
+def keyboard_input_fonc(pygame):
+    pressing = pygame.key.get_pressed()
+    #z
+    if pressing[pygame.K_z]:
+        keyboard_input["z"] = True
+    else:
+        keyboard_input["z"] = False
+        
+
+    #s
+    if pressing[pygame.K_s]:
+        keyboard_input["s"] = True
+    else:
+        keyboard_input["s"] = False
+        
+
+    #q
+    if pressing[pygame.K_q]:
+        keyboard_input["q"] = True
+    else:
+        keyboard_input["q"] = False
+        
+
+    #d
+    if pressing[pygame.K_d]:
+        keyboard_input["d"] = True
+    else:
+        keyboard_input["d"] = False
+        
+
+    #UP
+    if pressing[pygame.K_UP]:
+        keyboard_input["UP"] = True
+    else:
+        keyboard_input["UP"] = False
+        
+
+    #DOWN
+    if pressing[pygame.K_DOWN]:
+        keyboard_input["DOWN"] = True
+    else:
+        keyboard_input["DOWN"] = False
+        
+
+    #LEFT
+    if pressing[pygame.K_LEFT]:
+        keyboard_input["LEFT"] = True
+    else:
+        keyboard_input["LEFT"] = False
+        
+
+    #RIGHT
+    if pressing[pygame.K_RIGHT]:
+        keyboard_input["RIGHT"] = True
+    else:
+        keyboard_input["RIGHT"] = False
+        
+
+    #SPACE
+    if pressing[pygame.K_SPACE]:
+        keyboard_input["SPACE"] = True
+    else:
+        keyboard_input["SPACE"] = False
+        
+
+    #LSHIFT
+    if pressing[pygame.K_LSHIFT]:
+        keyboard_input["LSHIFT"] = True
+    else:
+        keyboard_input["LSHIFT"] = False
+        
+
+    #RETURN
+    if pressing[pygame.K_RETURN]:
+        keyboard_input["RETURN"] = True
+    else:
+        keyboard_input["RETURN"] = False
+        
+
+    #ESCAPE
+    if pressing[pygame.K_ESCAPE]:
+        keyboard_input["ESCAPE"] = True
+    else:
+        keyboard_input["ESCAPE"] = False
+
+    return keyboard_input
+
+
+key_name = [
+    "K_z",
+    "K_s",
+    "K_q",
+    "K_d",
+    "K_UP",
+    "K_DOWN",
+    "K_LEFT",
+    "K_RIGHT",
+    "K_SPACE",
+    "K_LSHIFT",
+    "K_RETURN",
+    "K_ESCAPE"
+]
