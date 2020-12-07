171a97c0dcd34f77e03967338f988c67760802c1
GitFenixZ <soantonyly@gmail.com>
2020-12-07 23:54:25 +0100

    Update Player by Tony

--- includes/player.py
+++ includes/player.py
@@ -1,5 +1,19 @@
 class Player():
 
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, sprite, pos_x, pos_y, length, width, speed):
+        
+        self.sprite = sprite
         self.x = pos_x
         self.y = pos_y
+        self.length = length
+        self.width = width
+        self.speed = speed
+
+# %%
+#test grille
+grille = []
+for x in range(15+1):
+    for y in range(13+1):
+        grille.append([x,y])
+
+print(grille)
